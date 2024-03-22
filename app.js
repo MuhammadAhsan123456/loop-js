83d7a0bac8a112d82d2b56f5739aac25d352eec7
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-03-22 18:21:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,129 @@
+
+
+// for(var i = 0; i < 51; i++){
+//     console.log("loop" + i);
+// }
+
+// var a = ["Muhammad Abu Bakar",
+// "Muhammad Ali",
+// "Muhammad Muaviya",
+// "Muhammad Hamza",
+// "Muhammad Ali",
+// "Muhammad usman",
+// ]
+
+// var b = "Muhammad Ali"
+
+// for(var i = 0; i < a.length; i++){
+//     if(a[i] == b){
+//         console.log("word found",a[i]);
+        
+//     }
+// }
+
+
+
+
+
+// var a = [
+//     "haris",
+//     "zain",
+//     "zaid",
+//     "fahad",
+//     "faizan",
+//     "umar",
+//     "ayan"  
+// ]
+// var b = "faiz"
+// var found = false
+
+
+// for(var i = 0; i < a.length ; i++){
+
+//     if(a[i] === b){
+//         console.log("Match",a[i])
+//     }
+//     if(a[i] === b){
+//         found = true
+//     }
+// }
+
+// if(found == true){
+//     console.log("found")
+// }else{
+//     console.log("not found")
+// }
+
+
+var a = [
+"Muhammad Ahsan",
+"Muhammad Bilal",
+"Muhammad yousouf",
+"Muhammad Umar",
+"Muhammad Usman",
+"Muhammad Ali",
+"Muhammad Talha"]
+
+var b = "Muhammad Aliyan"
+found = false
+
+for(var i = 0; i < a.length; i++){
+    if(a[i] === b){
+        found = true
+    }
+}
+
+if(found == true){
+    console.log("*Yes")
+}else{
+    console.log("No")
+}
+
+
+
+
+    
+
+
+
+
+
+// output
+// var c = ["1a","1b","1c","1d","1e","2a","2b","2bc" ....... "5e"]
+//  var a = ["a","b","c","d","e"]
+// var b = [1,2,3,4,5]
+
+// var c =[]
+
+// for(var i = 0; i < a.length ; i++){
+//     for(var j = 0; j < b.length ; j++){
+//         console.log(a[i] , b[j])
+//         c.push(a[i]+b[j])
+//     }
+// }
+// console.log(c)
+
+
+// var a = 1
+// console.log(a)
+// var b = 2
+// console.log(b)
+// var c = 3
+// console.log(c)
+// var d = 4
+// console.log(d)
+// var e = 5
+// console.log(e)
+
+
+// for(var i = 1; i <= 50; i++){
+//     document.write("Muhammad Ahsan")
+//     document.write("<br>")
+// }
+
+
+// for(var i = 50; i >= 1; i--){
+//     console.log(+i)
+// }
+
+
